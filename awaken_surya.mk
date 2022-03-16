b89ff53ae95814cc58176d7bd235c17e72289a30
aminuddinihsan20 <47616229+aminuddinihsan20@users.noreply.github.com>
2022-03-16 21:18:26 +0700

    awakeunBringup

--- awaken_surya.mk
+++ awaken_surya.mk
@@ -18,8 +18,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Nusantara stuff.
-$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+# Inherit some common Awaken stuff.
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
 
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -34,6 +34,7 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_GAPPS_ARCH := arm64
 USE_GAPPS := true
 
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Pixel Charging
 USE_PIXEL_CHARGING := true
@@ -41,7 +42,7 @@ USE_PIXEL_CHARGING := true
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-PRODUCT_NAME := spark_surya
+PRODUCT_NAME := awaken_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
